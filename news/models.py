1cbcfcbfd7cc4167b7f184a44c595a6e0642f3bc
Захаров Андрей <zaharov.andrey161@gmail.com>
2021-06-06 17:39:27 +0300

    Initial commit

---
+++ news/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
+
+
+class Article(models.Model):
+    """
+    A model that represents news report or some article
+    """
+    artcile_header = models.CharField(verbose_name='Article header', max_length=50)
+    artcile_text = models.TextField(verbose_name='Article text')
+    publication_date = models.DateField(verbose_name='Publication Date')
+    author = models.ForeignKey(verbose_name='Article author', to=User, on_delete=CASCADE)
+    likes = models.IntegerField(verbose_name="Likes on article", default=0)
