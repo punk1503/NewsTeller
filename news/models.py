2d24de753b9804b7dfbd76ec105e93490e917cb4
Захаров Андрей <zaharov.andrey161@gmail.com>
2021-06-07 18:37:22 +0300

    Changed article_title max length to 100

--- news/models.py
+++ news/models.py
@@ -11,10 +11,8 @@ class Article(models.Model):
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
 
-    article_title = models.CharField(
-        verbose_name='Article header', max_length=50)
+    article_title = models.CharField(verbose_name='Article header', max_length=100)
     article_text = models.TextField(verbose_name='Article text')
     publication_date = models.DateField(verbose_name='Publication Date')
-    author = models.ForeignKey(
-        verbose_name='Article author', to=User, on_delete=CASCADE)
+    author = models.ForeignKey(verbose_name='Article author', to=User, on_delete=CASCADE)
     likes = models.IntegerField(verbose_name="Likes on article", default=0)
